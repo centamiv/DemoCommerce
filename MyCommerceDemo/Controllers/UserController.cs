e667180591d443acb168d7377fd3f47adef4fd61
Ivan Centamori <ivan.centamori@outlook.com>
2021-01-16 09:15:05 +0100

    registrazione

--- MyCommerceDemo/Controllers/UserController.cs
+++ MyCommerceDemo/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using MyCommerceDemo.Database;
+using MyCommerceDemo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,11 @@ namespace MyCommerceDemo.Controllers
         }
 
         [HttpGet]
-        public ActionResult Login()
+        [ActionName("Login")]
+        public ActionResult LoginGet()
         {
-            return View();
+            var model = new LoginUserViewModel();
+            return View(model);
         }
 
         [HttpGet]
@@ -31,17 +34,18 @@ namespace MyCommerceDemo.Controllers
         }
 
         [HttpPost]
-        public ActionResult Check()
+        [ActionName("Login")]
+        public ActionResult LoginPost()
         {
             var username = Request["username"];
             var password = Request["password"];
 
-            var idaziendamaster = Const.IdAziendaMaster;
-
-            var utente = _db.tuteweb.Where(i => i.idaziendamaster == idaziendamaster).Where(i => i.mail == username || i.utente == username).Where(i => i.pswd == password).FirstOrDefault();
+            var utente = _db.tuteweb.Where(i => i.idaziendamaster == Const.IdAziendaMaster).Where(i => i.mail == username || i.utente == username).Where(i => i.pswd == password).FirstOrDefault();
             if (utente == null)
             {
-                return RedirectToAction("Login", "User");
+                var model = new LoginUserViewModel();
+                model.Message = "Nome utente o password non validi";
+                return View(model);
             }
 
             Session["User"] = utente;
@@ -49,9 +53,55 @@ namespace MyCommerceDemo.Controllers
         }
 
         [HttpGet]
-        public ActionResult Register()
+        [ActionName("Register")]
+        public ActionResult RegisterGet()
         {
-            return View();
+            var model = new RegisterUserViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Register")]
+        public ActionResult RegisterPost()
+        {
+            var nome = Request["nome"];
+            var cognome = Request["cognome"];
+            var email = Request["email"];
+            var password = Request["password"];
+            var passwordconfirm = Request["passwordconfirm"];
+
+
+            var model = new RegisterUserViewModel();
+
+            if (password != passwordconfirm)
+            {
+                model.Message = "Password non corrispondente";
+                return View(model);
+            }
+
+            var utente = _db.tuteweb.Where(i => i.idaziendamaster == Const.IdAziendaMaster)
+                .Where(i => i.mail == email || i.utente == email)
+                .FirstOrDefault();
+            if (utente != null)
+            {
+                model.Message = "Utente già registrato";
+                return View(model);
+            }
+
+            utente = new tuteweb();
+            utente.utente = email;
+            utente.mail = email;
+            utente.nome = nome;
+            utente.cognome = cognome;
+            utente.idaziendamaster = Const.IdAziendaMaster;
+            utente.pswd = password;
+            utente.idcliente = 0;
+            utente.Shopoweb = "Si";
+            _db.tuteweb.Add(utente);
+            _db.SaveChanges();
+
+            Session["User"] = utente;
+            return RedirectToAction("List", "Product");
         }
     }
 }
---
+++ MyCommerceDemo/Models/RegisterUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCommerceDemo.Models
+{
+    public class RegisterUserViewModel
+    {
+        public string Message { get; set; }
+        //public string Username { get; set; }
+        //public string Password { get; set; }
+    }
+}
